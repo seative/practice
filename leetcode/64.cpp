310fe1177b2f2c0dd30b109f9cb076924d2efd6b
seative <2521001170@qq.com>
2024-04-29 14:08:28 +0800

    modify style

--- leetcode/64.cpp
+++ leetcode/64.cpp
@@ -1,11 +1,9 @@
-// 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+// 给定一个包含非负整数的 m x n 网格 grid
+// ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
 
 // 说明：每次只能向下或者向右移动一步。
 
- 
-
 // 示例 1：
-
 
 // 输入：grid = [[1,3,1],[1,5,1],[4,2,1]]
 // 输出：7
@@ -19,31 +17,27 @@ using namespace std;
 
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
+  int minPathSum(vector<vector<int>> &grid) {
+    int m = grid.size();
+    int n = grid[0].size();
 
-        int dp[m][n];
-        dp[0][0]=grid[0][0];
+    int dp[m][n];
+    dp[0][0] = grid[0][0];
 
-        for(int i=1;i<m;i++)
-        {
-            dp[i][0]=grid[i][0]+dp[i-1][0];
-        }
-
-        for(int j=1;j<n;j++)
-        {
-            dp[0][j]=grid[0][j]+dp[0][j-1];
-        }
-
-        for(int i=1;i<m;++i)
-        {
-            for(int j=1;j<n;++j)
-            {
-                dp[i][j]=grid[i][j]+min(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-
-        return dp[m-1][n-1];      
+    for (int i = 1; i < m; i++) {
+      dp[i][0] = grid[i][0] + dp[i - 1][0];
     }
+
+    for (int j = 1; j < n; j++) {
+      dp[0][j] = grid[0][j] + dp[0][j - 1];
+    }
+
+    for (int i = 1; i < m; ++i) {
+      for (int j = 1; j < n; ++j) {
+        dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+
+    return dp[m - 1][n - 1];
+  }
 };
