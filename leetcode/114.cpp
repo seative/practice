76918d8f14819b36251560919c9b2a3e944bdb73
seative <2521001170@qq.com>
2024-06-05 15:23:33 +0800

    leetcode 114

--- leetcode/114.cpp
+++ leetcode/114.cpp
@@ -56,3 +56,24 @@ class Solution {
     }
   }
 };
+
+// O1 空间复杂度的解法
+class Solution2 {
+ public:
+  void flatten(TreeNode *root) {
+    TreeNode *curr = root;
+    while (curr != nullptr) {
+      if (curr->left != nullptr) {
+        auto next = curr->left;
+        auto predecessor = next;
+        while (predecessor->right != nullptr) {
+          predecessor = predecessor->right;
+        }
+        predecessor->right = curr->right;
+        curr->left = nullptr;
+        curr->right = next;
+      }
+      curr = curr->right;
+    }
+  }
+};
