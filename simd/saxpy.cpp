21a890674d7b5e3d138e67f1e9c7d4f6a7aa4fd0
seative <2521001170@qq.com>
2024-07-03 19:31:58 +0800

    update simd

--- simd/saxpy.cpp
+++ simd/saxpy.cpp
@@ -32,6 +32,28 @@
 }
 // END CODE
 
+// BEGIN CODE
+[[gnu::optimize("O2")]] void saxpy2(float a, float const *x, float const *y,
+                                    float *z, size_t n) {
+  size_t i;
+  __m128 av = _mm_set1_ps(a);
+  for (i = 0; i + 3 < n; i += 4) {
+    // __m128 xi = _mm_loadu_ps(&x[i]);
+    // __m128 yi = _mm_loadu_ps(&y[i]);
+    __m128 zi =
+        _mm_add_ps(_mm_mul_ps(av, _mm_loadu_ps(&x[i])), _mm_loadu_ps(&y[i]));
+    _mm_storeu_ps(&z[i], zi);
+  }
+  for (; i < n; i++) {
+    z[i] = a * x[i] + y[i];
+  }
+
+  // for (size_t i = 0; i < 7; ++i) {
+  //   z[i] = a * x[i] + y[i];
+  // }
+}
+// END CODE
+
 static void bench(benchmark::State &s) {
   const auto n = size_t(8192);
   auto a = 0.5F;
@@ -52,7 +74,30 @@ static void bench(benchmark::State &s) {
   }
   s.SetItemsProcessed(n * s.iterations());
 }
+
+static void bench2(benchmark::State &s) {
+  const auto n = size_t(8192);
+  auto a = 0.5F;
+  auto x = std::vector<float>(n);
+  auto y = std::vector<float>(n);
+  auto z = std::vector<float>(n);
+  auto z2 = std::vector<float>(n);
+  std::generate(x.begin(), x.end(),
+                [uni = std::uniform_real_distribution<float>(),
+                 rng = std::mt19937()]() mutable { return uni(rng); });
+  std::generate(y.begin(), y.end(),
+                [uni = std::uniform_real_distribution<float>(),
+                 rng = std::mt19937()]() mutable { return uni(rng); });
+  for (auto _ : s) {
+    saxpy2(a, x.data(), y.data(), z.data(), n);
+    benchmark::DoNotOptimize(x);
+    benchmark::DoNotOptimize(y);
+  }
+  s.SetItemsProcessed(n * s.iterations());
+}
+
 BENCHMARK(bench);
+BENCHMARK(bench2);
 
 void scalar_saxpy(float a, float const *x, float const *y, float *z, size_t n) {
   for (size_t i = 0; i < n; i++) {
