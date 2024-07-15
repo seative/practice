f1e9050f20a64534d4ee8237387e983cbca50567
seative <2521001170@qq.com>
2024-07-15 18:00:48 +0800

    leetcode 283 287

---
+++ leetcode/287.cpp
@@ -0,0 +1,73 @@
+// 给定一个包含 n + 1 个整数的数组 nums ，其数字都在 [1, n] 范围内（包括 1 和
+// n），可知至少存在一个重复的整数。
+
+// 假设 nums 只有 一个重复的整数 ，返回 这个重复的数 。
+
+// 你设计的解决方案必须 不修改 数组 nums 且只用常量级 O(1) 的额外空间。
+
+// 示例 1：
+
+// 输入：nums = [1,3,4,2,2]
+// 输出：2
+// 示例 2：
+
+// 输入：nums = [3,1,3,4,2]
+// 输出：3
+// 示例 3 :
+
+// 输入：nums = [3,3,3,3,3]
+// 输出：3
+
+// 提示：
+
+// 1 <= n <= 105
+// nums.length == n + 1
+// 1 <= nums[i] <= n
+// nums 中 只有一个整数 出现 两次或多次 ，其余整数均只出现 一次
+#include <bitset>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  int findDuplicate(vector<int>& nums) {
+    int a = nums.size();
+    vector<char> arr(a / 8 + 1, 0);
+    for (int i = 0; i < a; ++i) {
+      int m = nums[i] / 8;
+      int n = nums[i] % 8;
+      if (arr[m] & (1 << n)) {
+        return nums[i];
+      }
+      arr[m] |= 1 << n;
+    }
+    return 0;
+  }
+};
+
+// 二分法
+class Solution2 {
+ public:
+  int findDuplicate(vector<int>& nums) {
+    int l = 0;
+    int r = nums.size() - 1;
+    while (l < r) {
+      int mid = (l + r) >> 1;
+      int cnt = 0;
+      for (int i = 0; i < nums.size(); ++i) {
+        if (nums[i] <= mid) {
+          cnt++;
+        }
+      }
+      if (cnt <= mid) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    }
+    return nums[l];
+  }
+};
+
+// 二进制
+// 环形链表
