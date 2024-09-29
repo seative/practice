1a9edbec01fb1f45c6689d0445d46e2c2992a060
seative <2521001170@qq.com>
2024-09-29 14:01:45 +0800

    leetcode 377

--- leetcode/322.cpp
+++ leetcode/322.cpp
@@ -42,28 +42,45 @@ using namespace std;
 // };
 
 // 记忆化搜索
-class Solution {
-  vector<int> count;
-  int dp(vector<int>& coins, int rem) {
-    if (rem < 0) return -1;
-    if (rem == 0) return 0;
-    if (count[rem - 1] != 0) return count[rem - 1];
-    int Min = INT_MAX;
-    for (int coin : coins) {
-      int res = dp(coins, rem - coin);
-      if (res >= 0 && res < Min) {
-        Min = res + 1;
-      }
-    }
-    count[rem - 1] = Min == INT_MAX ? -1 : Min;
-    return count[rem - 1];
-  }
+// class Solution {
+//   vector<int> count;
+//   int dp(vector<int>& coins, int rem) {
+//     if (rem < 0) return -1;
+//     if (rem == 0) return 0;
+//     if (count[rem - 1] != 0) return count[rem - 1];
+//     int Min = INT_MAX;
+//     for (int coin : coins) {
+//       int res = dp(coins, rem - coin);
+//       if (res >= 0 && res < Min) {
+//         Min = res + 1;
+//       }
+//     }
+//     count[rem - 1] = Min == INT_MAX ? -1 : Min;
+//     return count[rem - 1];
+//   }
 
+//  public:
+//   int coinChange(vector<int>& coins, int amount) {
+//     if (amount < 1) return 0;
+//     count.resize(amount);
+//     return dp(coins, amount);
+//   }
+// };
+
+class Solution {
  public:
   int coinChange(vector<int>& coins, int amount) {
-    if (amount < 1) return 0;
-    count.resize(amount);
-    return dp(coins, amount);
+    vector<int> dp(amount + 1, amount + 1);
+
+    dp[0] = 0;
+    for (int i = 1; i <= amount; ++i) {
+      for (int j = 0; j < coins.size(); ++j) {
+        if (coins[j] < amount) {
+          dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+        }
+      }
+    }
+    return dp[amount] == amount + 1 ? -1 : dp[amount];
   }
 };
 
---
+++ leetcode/377.cpp
@@ -0,0 +1,53 @@
+// 小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为 root 。
+
+// 除了 root
+// 之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。
+// 如果 两个直接相连的房子在同一天晚上被打劫 ，房屋将自动报警。
+
+// 给定二叉树的 root 。返回 在不触动警报的情况下 ，小偷能够盗取的最高金额 。
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+
+#include <cmath>
+#include <map>
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  int rob(TreeNode *root) {
+    auto [not_rob, rob] = dp(root);
+    return max(not_rob, rob);
+  }
+
+  std::pair<int, int> dp(TreeNode *root) {
+    if (root == nullptr) return {0, 0};
+    if (root->left == nullptr && root->right == nullptr) return {root->val, 0};
+
+    auto [left_rob, left_not_rob] = dp(root->left);
+    auto [right_rob, right_not_rob] = dp(root->right);
+
+    int not_rob = max(left_rob, left_not_rob) + max(right_rob, right_not_rob);
+    int rob = root->val + left_not_rob + right_not_rob;
+    return {rob, not_rob};
+  }
+};
