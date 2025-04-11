3e047998a1a3d45a01ec663481661db8d103da79
seative <2521001170@qq.com>
2025-04-11 16:26:55 +0800

    modify

--- leetcode/309.cpp
+++ leetcode/309.cpp
@@ -32,3 +32,23 @@ class Solution {
     return max(f[n - 1][1], f[n - 1][2]);
   }
 };
+
+// 买卖股票最佳时机2
+class Solution2 {
+ public:
+  int maxProfit(vector<int>& prices) {
+    if (prices.empty()) {
+      return 0;
+    }
+
+    int n = prices.size();
+    vector<vector<int>> f(n, vector<int>(2));
+    f[0][1] = -prices[0];
+    for (int i = 1; i < n; ++i) {
+      f[i][0] = max(f[i - 1][0], f[i - 1][1] + prices[i]);
+      f[i][1] = max(f[i - 1][1], f[i - 1][0] - prices[i]);
+    }
+
+    return f[n - 1][0];
+  }
+};
